2744ceb42cd04d6cb98c30dce5d9417caea2f0b0
fadelalif <126957890+fadelalif@users.noreply.github.com>
2023-03-24 09:12:10 +0700

    FINAL

--- jurnalmod5_1302210059/Program.cs
+++ jurnalmod5_1302210059/Program.cs
@@ -1,5 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using jurnalmod5_1302210059;
+using static jurnalmod5_1302210059.Penjumlahan;
+
+long a = 13;
+long b = 02;
+long c = 21;
+JumlahTigaAngka<long> data = new JumlahTigaAngka<long>(a, b, c);
+data.PrintData();
 
 SimpleDataBase<int> dataBase= new SimpleDataBase<int>();
 
