6b886a1368f233df8a6c83b8eebc1de5a56fafaa
fadelalif <126957890+fadelalif@users.noreply.github.com>
2023-03-24 08:15:26 +0700

    “menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ jurnalmod5_1302210059/Penjumlahan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurnalmod5_1302210059
+{
+    internal class Penjumlahan
+    {
+        public class JumlahTigaAngka<T>
+        {
+            private T a, b, c, hasil;
+
+            public JumlahTigaAngka(T a, T b, T c)
+            {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+            }
+
+            public void PrintData()
+            {
+                long hasil = (dynamic)a+(dynamic)b+(dynamic)c;
+                Console.WriteLine("Hasil perjumlahan adalah: " + hasil);
+            }
+        }
+    }
+}
--- jurnalmod5_1302210059/Program.cs
+++ jurnalmod5_1302210059/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using static jurnalmod5_1302210059.Penjumlahan;
+
+long a = 13;
+long b = 02;
+long c = 21;
+JumlahTigaAngka<long> data = new JumlahTigaAngka<long>(a, b, c);
+data.PrintData();
