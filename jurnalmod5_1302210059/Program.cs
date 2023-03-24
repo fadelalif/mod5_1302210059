d52a3dad701a14b7c42c204d275afb8771c8d139
fadelalif <126957890+fadelalif@users.noreply.github.com>
2023-03-24 09:02:21 +0700

    menambahkan class SimpleDataBase

--- jurnalmod5_1302210059/Program.cs
+++ jurnalmod5_1302210059/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using jurnalmod5_1302210059;
+
+SimpleDataBase<int> dataBase= new SimpleDataBase<int>();
+
+dataBase.AddNewData(13);
+dataBase.AddNewData(02);
+dataBase.AddNewData(21);
+dataBase.printAllData();
